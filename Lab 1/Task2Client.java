e19346605cb8085b1f8501c84ecf78d6d7627500
Justin K -School <justinjoseph0007@gmail.com>
2019-09-17 15:36:51 -0400

    Re-indent

--- Lab 1/Task2Client.java
+++ Lab 1/Task2Client.java
@@ -3,42 +3,38 @@ import java.io.DataInputStream;
 import java.net.*;
 
 public class Task2Client {
-
-	/**
-	 * @param args
-	 */
-	@SuppressWarnings("deprecation")
-	public static void main(String[] args) throws Exception {
+    @SuppressWarnings("deprecation")
+    public static void main(String[] args) throws Exception {
 		
-		try {
-	        Socket client=new Socket("127.0.0.1",7896); 
-	        DataInputStream r=new DataInputStream(client.getInputStream()); 
-	        PrintStream w=new PrintStream(client.getOutputStream()); 
+	try {
+	    Socket client=new Socket("127.0.0.1",7896); 
+	    DataInputStream r=new DataInputStream(client.getInputStream()); 
+	    PrintStream w=new PrintStream(client.getOutputStream()); 
 	       
-	        //get input from keyboard
-	        System.out.println("Enter text to decypher");
-	        DataInputStream in=new DataInputStream(System.in); 
-			String text = in.readLine();
+	    //get input from keyboard
+	    System.out.println("Enter text to decypher");
+	    DataInputStream in=new DataInputStream(System.in); 
+	    String text = in.readLine();
 			
-	        //send number to server
-	        w.println(text);
-	        
-	        System.out.println("Enter number to shift by");
-	        String number = in.readLine();
-	        w.write(Integer.parseInt(number));
-	        
-	        //read answer from server and print it
-	        String answer = r.readLine();
-	        System.out.println(answer);
-	        
-			}catch (UnknownHostException e){
-				System.out.println("Sock:"+e.getMessage()); 
-			}catch (EOFException e){
-				System.out.println("EOF:"+e.getMessage());
-			}catch (IOException e){
-				System.out.println("IO:"+e.getMessage());
-			}
-
+	    //send number to server
+	    w.println(text);
+		
+	    System.out.println("Enter number to shift by");
+	    String number = in.readLine();
+	    w.write(Integer.parseInt(number));
+		
+	    //read answer from server and print it
+	    String answer = r.readLine();
+	    System.out.println(answer);
+		
+	}catch (UnknownHostException e){
+	    System.out.println("Sock:"+e.getMessage()); 
+	}catch (EOFException e){
+	    System.out.println("EOF:"+e.getMessage());
+	}catch (IOException e){
+	    System.out.println("IO:"+e.getMessage());
 	}
+
+    }
 
 }
--- Lab 1/Task2Server.java
+++ Lab 1/Task2Server.java
@@ -3,52 +3,48 @@ import java.io.DataInputStream;
 import java.net.*; 
 
 public class Task2Server {
-
-	/**
-	 * @param args
-	 */
-	@SuppressWarnings("deprecation")
-	public static void main(String[] args) throws Exception {
+    @SuppressWarnings("deprecation")
+    public static void main(String[] args) throws Exception {
 		
-		ServerSocket server;
-		DataInputStream in;
-		PrintStream w;
-		DataInputStream r;
-        
-		try {
-		server=new ServerSocket(7896);  
-        Socket client=server.accept(); 
-        r=new DataInputStream(client.getInputStream()); 
-        w=new PrintStream(client.getOutputStream()); 
-        //in=new DataInputStream(System.in); 
-        
-        String text = r.readLine();
-        System.out.println(text);
-        int number = r.read();
-        System.out.println(number);
-        
-        StringBuffer result= new StringBuffer();
+	ServerSocket server;
+	DataInputStream in;
+	PrintStream w;
+	DataInputStream r;
+	
+	try {
+	    server=new ServerSocket(7896);  
+	    Socket client=server.accept(); 
+	    r=new DataInputStream(client.getInputStream()); 
+	    w=new PrintStream(client.getOutputStream()); 
+	    //in=new DataInputStream(System.in); 
+	
+	    String text = r.readLine();
+	    System.out.println(text);
+	    int number = r.read();
+	    System.out.println(number);
+	
+	    StringBuffer result= new StringBuffer();
        
-        for (int i=0; i<text.length(); i++) 
-        { 
-            if (Character.isUpperCase(text.charAt(i))) 
-            { 
-                char ch = (char)(((int)text.charAt(i) + number - 65) % 26 + 65); 
-                result.append(ch); 
-            } 
-            else
-            { 
-                char ch = (char)(((int)text.charAt(i) + number - 97) % 26 + 97); 
-                result.append(ch); 
-            } 
-        } 
-        
-        w.println(result);
-        
-		} catch(IOException e)  {
-				System.out.println("Connection:"+e.getMessage());
-		}
-
+	    for (int i=0; i<text.length(); i++) 
+		{ 
+		    if (Character.isUpperCase(text.charAt(i))) 
+			{ 
+			    char ch = (char)(((int)text.charAt(i) + number - 65) % 26 + 65); 
+			    result.append(ch); 
+			} 
+		    else
+			{ 
+			    char ch = (char)(((int)text.charAt(i) + number - 97) % 26 + 97); 
+			    result.append(ch); 
+			} 
+		} 
+	
+	    w.println(result);
+	
+	} catch(IOException e)  {
+	    System.out.println("Connection:"+e.getMessage());
 	}
+
+    }
 
 }
