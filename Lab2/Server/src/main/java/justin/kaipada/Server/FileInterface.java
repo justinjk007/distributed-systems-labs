37bf8bba05f89d3ade2d54ed2e3fb1a76783dbee
Justin K -Linux <justinjoseph0007@gmail.com>
2019-10-01 14:41:53 -0400

    Trying to ignore target folders

--- Lab2/Server/src/main/java/justin/kaipada/Server/FileInterface.java
+++ Lab2/Server/src/main/java/justin/kaipada/Server/FileInterface.java
@@ -4,5 +4,6 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface FileInterface extends Remote {
-   public byte[] downloadFile(String fileName) throws RemoteException;
+    public byte[] downloadFile(String fileName) throws RemoteException;
+    public void uploadFile(byte[] content) throws RemoteException;
 }
