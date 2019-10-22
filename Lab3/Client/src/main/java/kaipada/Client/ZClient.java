b0f171b4b1781dc1425f41079213dc62e78b7c9a
Justin K -Linux <justinjoseph0007@gmail.com>
2019-10-22 11:47:38 -0400

    Working example from jeromq

--- Lab3/Client/src/main/java/kaipada/Client/ZClient.java
+++ Lab3/Client/src/main/java/kaipada/Client/ZClient.java
@@ -1,17 +1,28 @@
 package kaipada.Client;
 
+import org.zeromq.SocketType;
 import org.zeromq.ZMQ;
+import org.zeromq.ZContext;
 
 public class ZClient {
 
-    public static void main(String[] args) {
-        ZMQ.Context context = ZMQ.context(1);
+    public static void main(String[] args)
+    {
+        try (ZContext context = new ZContext()) {
+            System.out.println("Connecting to hello world server");
 
+	    //  Socket to talk to server
+            ZMQ.Socket socket = context.createSocket(SocketType.REQ);
+            socket.connect("tcp://localhost:5555");
 
-        ZMQ.Socket requester = context.socket(ZMQ.REQ);
-        requester.connect("tcp://localhost:5555");
+            for (int requestNbr = 0; requestNbr != 10; requestNbr++) {
+                String request = "Hello";
+                System.out.println("Sending Hello " + requestNbr);
+                socket.send(request.getBytes(ZMQ.CHARSET), 0);
 
-        requester.close();
-        context.term();
+                byte[] reply = socket.recv(0);
+                System.out.println("Received " + new String(reply, ZMQ.CHARSET) + " " +requestNbr);
+            }
+        }
     }
 }
--- Lab3/Server/src/main/java/kaipada/Server/ZServer.java
+++ Lab3/Server/src/main/java/kaipada/Server/ZServer.java
@@ -1,17 +1,24 @@
 package kaipada.Server;
 
+import org.zeromq.SocketType;
 import org.zeromq.ZMQ;
+import org.zeromq.ZContext;
 
 public class ZServer {
 
-    public static void main(String[] args) throws Exception {
-        ZMQ.Context context = ZMQ.context(1);
+    public static void main(String[] args) throws Exception
+    {
+	try (ZContext context = new ZContext()) {
+	    //  Socket to talk to clients
+	    ZMQ.Socket socket = context.createSocket(SocketType.REP);
+	    socket.bind("tcp://*:5555");
 
-        ZMQ.Socket responder = context.socket(ZMQ.REP);
-        responder.bind("tcp://*:5555");
-
- 
-        responder.close();
-        context.term();
-    }
-}
+	    while (!Thread.currentThread().isInterrupted()) {
+		byte[] reply = socket.recv(0);
+		System.out.println("Received " + ": [" + new String(reply, ZMQ.CHARSET) + "]");
+		String response = "world";
+		socket.send(response.getBytes(ZMQ.CHARSET), 0);
+		Thread.sleep(1000); //  Do some 'work'
+	    }
+	}
+    }}
