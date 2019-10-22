1ac2d694f8d66fe0c916d1dd41490d903338b438
Justin K -Linux <justinjoseph0007@gmail.com>
2019-10-22 11:26:11 -0400

    Initial setup with maven

---
+++ Lab3/Client/src/main/java/kaipada/Client/ZClient.java
@@ -0,0 +1,17 @@
+package kaipada.Client;
+
+import org.zeromq.ZMQ;
+
+public class ZClient {
+
+    public static void main(String[] args) {
+        ZMQ.Context context = ZMQ.context(1);
+
+
+        ZMQ.Socket requester = context.socket(ZMQ.REQ);
+        requester.connect("tcp://localhost:5555");
+
+        requester.close();
+        context.term();
+    }
+}
